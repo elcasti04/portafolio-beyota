af2806bd20e9097f8f76966409b7d07e824049df
AndresCastro <188876553+elcasti04@users.noreply.github.com>
2025-08-27 10:26:16 -0500

    Agregando nuevas secciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,25 +12,27 @@ parallax();
 activeMenu();
 sendEmail();
 
-document.querySelectorAll('.navbar__menu .navbar__list a').forEach(link => {
-  link.addEventListener('click', () => {
-    if(window.innerWidth <= 768){
-      window.location.hash = '';
-    }
-  });
+document.querySelectorAll('.navbar__menu .navbar__list a').forEach((link) => {
+	link.addEventListener('click', () => {
+		if (window.innerWidth <= 768) {
+			window.location.hash = '';
+		}
+	});
 });
 
 const menu = document.getElementById('nav');
 const openBtn = document.querySelector('.btn__open');
 const closeBtn = document.querySelector('.btn__close');
 
-if(openBtn && menu){
-  openBtn.addEventListener('click', () => {
-    document.body.classList.add('menu-open');
-  });
+if (openBtn && menu) {
+	openBtn.addEventListener('click', () => {
+		document.body.classList.add('menu-open');
+	});
 }
-if(closeBtn && menu){
-  closeBtn.addEventListener('click', () => {
-    document.body.classList.remove('menu-open');
-  });
+if (closeBtn && menu) {
+	closeBtn.addEventListener('click', (e) => {
+		e.preventDefault(); // Evita que el enlace navegue
+		document.body.classList.remove('menu-open');
+		menu.classList.remove('active'); // Opcional: si usas una clase para mostrar/ocultar el menú
+	});
 }
