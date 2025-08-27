96ddfb10f8723f82f5a3d742b459c38a0b0d0942
AndresCastro <188876553+elcasti04@users.noreply.github.com>
2025-08-27 10:24:38 -0500

    Agregando nuevas secciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,32 @@ import sendEmail from './components/send_form.js';
 import dateUpdater from './helpers/date_updater.js';
 import resetToHome from './helpers/reload_page.js';
 
-
-
 loader();
 resetToHome();
 dateUpdater();
 parallax();
 activeMenu();
 sendEmail();
+
+document.querySelectorAll('.navbar__menu .navbar__list a').forEach(link => {
+  link.addEventListener('click', () => {
+    if(window.innerWidth <= 768){
+      window.location.hash = '';
+    }
+  });
+});
+
+const menu = document.getElementById('nav');
+const openBtn = document.querySelector('.btn__open');
+const closeBtn = document.querySelector('.btn__close');
+
+if(openBtn && menu){
+  openBtn.addEventListener('click', () => {
+    document.body.classList.add('menu-open');
+  });
+}
+if(closeBtn && menu){
+  closeBtn.addEventListener('click', () => {
+    document.body.classList.remove('menu-open');
+  });
+}
